d8903cce5efa0ab390c708c57202d4f2f888e4f2
microelly2 <thomas@freecadbuch.de>
2015-04-09 18:57:12 +0200

    hinge joint and  telescope boom added

--- transform.py
+++ transform.py
@@ -7,11 +7,11 @@
 #-- GNU Lesser General Public License (LGPL)
 #-------------------------------------------------
 
-import FreeCAD,PySide,os
+import FreeCAD,PySide,os,FreeCADGui
 from PySide import QtCore, QtGui, QtSvg
 from PySide.QtGui import * 
 
-__vers__='0.1'
+__vers__='0.2'
 
 
 try:
@@ -281,16 +281,20 @@ class _ViewProviderTransform(object):
    
 	def __init__(self,vobj):
 		say("__init__" + str(self))
+		self.Object = vobj.Object
 		vobj.Proxy = self
 
 	def attach(self,vobj):
 		say("attach " + str(vobj.Object.Label))
 		self.Object = vobj.Object
 		self.obj2=self.Object
+		#self.dialog=TransformWidget(self)
 		#FreeCAD.t=self
 		if not hasattr(self.Object,"Lock"):
 				self.Object.Proxy.Lock=False
 				say("lock gesetzt")
+		FreeCAD.ty=self
+		FreeCAD.tv=vobj
 		return
 
 	def claimChildren(self):
@@ -307,7 +311,9 @@ class _ViewProviderTransform(object):
 	def setEdit(self,vobj,mode=0):
 		s=TransformWidget(self)
 		self.dialog=s
-		s.show()
+		self.dialog.show()
+		say("set Edit")
+		#s.show()
 		return True
 
 	def unsetEdit(self,vobj,mode=0):
@@ -324,11 +330,234 @@ class _ViewProviderTransform(object):
 		action.triggered.connect(self.edit)
 
 	def edit(self):
-		s=TransformWidget(self)
-		self.dialog=s
-		s.show()
+		self.dialog=TransformWidget(self)
+		self.dialog.show()
+		
 
 	def showVersion(self):
 		QtGui.QMessageBox.information(None, "About Transform", "Transform Node\n2015 microelly\nVersion " + __vers__ +"\nstill very alpha")
 
+
+
+#-----------------------
+
+class HingeWidget(QtGui.QWidget):
+	def __init__(self, obj,*args):
+		QtGui.QWidget.__init__(self, *args)
+
+		self.Object=obj
+		layout = QtGui.QGridLayout()
+#		self.vollabel = QtGui.QLabel(obj.Object.Label)
+#		layout.addWidget(self.vollabel, 0, 0)
+		self.dial = QtGui.QDial()
+		self.dial.setMaximum(360)
+		self.dial.setSingleStep(1)
+		self.dial.setNotchesVisible(True)
+		self.dial.valueChanged.connect(self.paramValueCanged);
+		layout.addWidget(self.dial, 10, 0,1,4)
+		self.setLayout(layout)
+		self.setWindowTitle("Hinge Joint")
+
+	def paramValueCanged(self):
+		say("Angel ValueCanged")
+		# say(self.dial.value())
+		v=self.dial.value()
+		say(v)
+		'''
+		say(self.Object.Object.Placement.Rotation.Angle)
+		say(self.Object.Object.Placement.Rotation.Axis)
+		self.Object.Object.Placement.Rotation=FreeCAD.Rotation(self.Object.Object.axis,v)
+		saye(self.Object.Object.Placement.Rotation.Angle)
+		saye(self.Object.Object.Placement.Rotation.Axis)
+		
+		say(self.Object.Object.Placement)
+		self.Object.Object.Proxy.execute(t)
+		'''
+		#---------------------------------------------
+		
+		FreeCAD.tt=self
+#		say(self.Object.Object.Placement)
+		if True:
+			qalt=self.Object.Object.plOld
+			qai=qalt.inverse()
+#			say(qai)
+			vt=self.Object.Object.axisAngle
+			say("vt=")
+			say(vt)
+			qt=FreeCAD.Placement()
+			qt.Rotation=FreeCAD.Rotation(self.Object.Object.axis,vt)
+#			say(qt)
+			vti=qt.inverse()
+			# rest=vti.multiply(qalt)
+			rest=qalt.multiply(vti)
+			say("rest=")
+			say(rest)
+			qn=FreeCAD.Placement()
+			#qn.Base=FreeCAD.Vector(self.Object.Object.direction).multiply(v)
+			qn.Rotation=FreeCAD.Rotation(self.Object.Object.axis,v)
+			say("neue pos einfach ... qn2=")
+			qn2=FreeCAD.Placement(qn)
+			rest2=FreeCAD.Placement(rest)
+			say(qn2)
+			
+			say("qneu ... zz=")
+			#FreeCAD.rest=rest
+			#FreeCAD.qn=qn
+			#zz=FreeCAD.rest.multiply(FreeCAD.qn)
+			#say(zz)
+			say("lokal berechnet:")
+			zz=rest.multiply(qn)
+			say(zz)
+			
+			self.Object.Object.axisAngle=v
+			self.Object.Object.Placement=zz
+			saye("fertig")
+			FreeCAD.ActiveDocument.recompute()
+		
+		
+		#----------------------------------------------
+		FreeCAD.ActiveDocument.recompute()
+
+
+class TelescopWidget(QtGui.QWidget):
+	def __init__(self, obj,*args):
+		QtGui.QWidget.__init__(self, *args)
+		self.Object=obj
+		layout = QtGui.QGridLayout()
+#		self.vollabel = QtGui.QLabel("Telescopic boom")
+#		layout.addWidget(self.vollabel, 0, 0)
+		self.slider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+		self.slider.setMaximum(100)
+		self.slider.valueChanged.connect(self.paramValueCanged);
+		layout.addWidget(self.slider, 12, 0,1,4)
+		self.setLayout(layout)
+		self.setWindowTitle("Telecopic Boom")
+
+	def paramValueCanged(self):
+		saye("ValueCanged")
+		say(self.slider.value())
+		v=self.slider.value()
+		FreeCAD.tt=self
+#		say(self.Object.Object.Placement)
+		if True:
+			qalt=self.Object.Object.plOld
+			qai=qalt.inverse()
+#			say(qai)
+			vt=self.Object.Object.axisScale
+#			say("vt=")
+#			say(vt)
+			qt=FreeCAD.Placement()
+			qt.Base=FreeCAD.Vector(self.Object.Object.direction).multiply(vt)
+#			say(qt)
+			vti=qt.inverse()
+			# rest=vti.multiply(qalt)
+			rest=qalt.multiply(vti)
+			say("rest=")
+			say(rest)
+			qn=FreeCAD.Placement()
+			qn.Base=FreeCAD.Vector(self.Object.Object.direction).multiply(v)
+			say("neue pos einfach ... qn2=")
+			qn2=FreeCAD.Placement(qn)
+			rest2=FreeCAD.Placement(rest)
+			say(qn2)
+			
+			say("qneu ... zz=")
+			#FreeCAD.rest=rest
+			#FreeCAD.qn=qn
+			#zz=FreeCAD.rest.multiply(FreeCAD.qn)
+			#say(zz)
+			say("lokal berechnet:")
+			zz=rest.multiply(qn)
+			say(zz)
+			
+			self.Object.Object.axisScale=v
+			self.Object.Object.Placement=zz
+			saye("fertig")
+			FreeCAD.ActiveDocument.recompute()
+
+
+class _ViewProviderHinge(_ViewProviderTransform):
+	def setupContextMenu(self, obj, menu):
+#		action = menu.addAction("About Transform ")
+#		action.triggered.connect(self.showVersion)
+		action = menu.addAction("Edit ... ")
+		action.triggered.connect(self.edit)
+		action = menu.addAction("Config ... ")
+		action.triggered.connect(self.config)
+
+	def config(self):
+		self.dialog=HingeWidget(self)
+		# self.dialog.show()
+		
+		mw = FreeCADGui.getMainWindow()
+		dock = QtGui.QDockWidget(self.Object.Label, mw)
+		dock.setStyleSheet("background-color:lightblue;color:blue;")
+		mw.centralWidget = QtGui.QWidget(dock)
+		mw.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
+		dock.setWidget(self.dialog)
+		say("widget set")
+
+
+	def doubleClicked(self,vobj):
+		self.setEdit(vobj)
+
+	def setEdit(self,vobj,mode=0):
+		self.config()
+		saye("edit ..")
+		return True
+
+
+class _ViewProviderTelescope(_ViewProviderTransform):
+
+	def getIcon(self):
+		return __dir__ +'/icons/mars.png'
+ 
+	def setupContextMenu(self, obj, menu):
+#		action = menu.addAction("About Transform B")
+#		action.triggered.connect(self.showVersion)
+		action = menu.addAction("Edit ... ")
+		action.triggered.connect(self.edit)
+		action = menu.addAction("Config ... ")
+		action.triggered.connect(self.config)
+
+	def config(self):
+		self.dialog=TelescopWidget(self)
+		#self.dialog.show()
+		mw = FreeCADGui.getMainWindow()
+		#dock = QtGui.QDockWidget("Mein Dock", mw)
+		dock = QtGui.QDockWidget(self.Object.Label, mw)
+		dock.setStyleSheet("background-color:yellow;color:brown;")
+		mw.centralWidget = QtGui.QWidget(dock)
+		mw.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
+		dock.setWidget(self.dialog)
+
+	def doubleClicked(self,vobj):
+		self.setEdit(vobj)
+
+	def setEdit(self,vobj,mode=0):
+		self.config()
+		saye("edit ..")
+		return True
+
+
+def createHinge(name='MyHinge', targets=[],src=None):
+	obj=createTransform(name, targets=[],src=None)
+	obj.addProperty("App::PropertyVector","axis","2 MyCTL","")
+	obj.axis=FreeCAD.Vector(0,0,1)
+	obj.addProperty("App::PropertyFloat","axisAngle","2 MyCTL","")
+	obj.axisAngle=0.0
+	_ViewProviderHinge(obj.ViewObject)
+	return obj
+
+def createTelescope(name='MyTelescope', targets=[],src=None):
+	obj=createTransform(name, targets=[],src=None)
+	obj.addProperty("App::PropertyVector","direction","2 MyCTL","")
+	obj.direction=FreeCAD.Vector(0,0,1)
+	obj.addProperty("App::PropertyFloat","axisScale","2 MyCTL","")
+	obj.axisScale=0.0
+
+	_ViewProviderTelescope(obj.ViewObject)
+	return obj
+
+#---------------------------------
 
